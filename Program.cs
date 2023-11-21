314dbb926994e745710b8823903c9c19566075a0
kyle- <kyle-smith1@outlook.com>
2023-11-21 20:49:26 +0000

    Challenge change finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿while (true) {
+﻿// CS50 Week 1
+// Mario - More Comfortable Challenge 
+/*
+while (false) {
     Console.WriteLine("Choose a pyramid height.");
     int userInput = Convert.ToInt32(Console.ReadLine());
     if (userInput > 0 && userInput < 9)
@@ -26,5 +29,74 @@
         }
     }
 }
-    
+*/
+// Cash Challenge - Greedy algorithm 
 
+
+int change = 0;
+
+
+while (true)
+{
+    int coins = 0;
+    int quarter = 0;
+    int dime = 0;
+    int nickel = 0;
+    int penny = 0;
+
+    do
+    {
+        Console.Write("Enter change amount..");
+        change = Convert.ToInt32(Console.ReadLine());
+    } while (change <= 0);
+
+
+    while (change > 0)
+    {
+        if (change >= 25)
+        {
+            while (change >= 25)
+            {
+                change = change - 25;
+                coins++;
+                quarter++;
+            }
+
+        }
+        if (change >= 10)
+        {
+            while (change >= 10)
+            {
+                change = change - 10;
+                coins++;
+                dime++;
+            }
+        }
+        if (change >= 5)
+        {
+            while (change >= 5)
+            {
+                change = change - 5;
+                coins++;
+                nickel++;
+            }
+        }
+        if (change >= 1)
+        {
+            while (change >= 1)
+            {
+                change = change - 1;
+                coins++;
+                penny++;
+            }
+        }
+
+    }
+
+    Console.WriteLine($" You should use {coins}coins " +
+        $"\n{quarter} Quarter " +
+        $"\n{dime} Dime " +
+        $"\n{nickel} Nickel " +
+        $"\n{penny} Penny");
+    
+}
