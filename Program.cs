f8ec3c2fa3b1b10d4ee6e745abb6136f22bafc5b
kyle- <kyle-smith1@outlook.com>
2023-11-21 19:19:46 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿while (true) {
+    Console.WriteLine("Choose a pyramid height.");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    if (userInput > 0 && userInput < 9)
+    {
+        int pyramidHeight = userInput;
+        int n = pyramidHeight;
+        int m = pyramidHeight;
+        int p = 0;
+        int q = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Console.Write(" ");
+            }
+            m--;
+            for (int k = 0; k <= p; k++) {
+                Console.Write("#");
+            }
+            p++;
+            Console.Write("  ");
+            for (int l = 0; l <= q; l++) {
+                Console.Write("#");
+            }
+            q++;
+            Console.WriteLine();
+        }
+    }
+}
+    
+
